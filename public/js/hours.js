d1a2ecc30741ef2f756bf0c9ddff0174d2f6958a
KAPILDHARIKA <55614013+KAPILDHARIKA@users.noreply.github.com>
2021-02-10 14:01:58 -0500

    Add files via upload

---
+++ public/js/hours.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    var hoursform = $("#updatehoursform"),
+     userNameInput = $("#UsernameEmp"),
+     startInput = $("#Startdate"),
+     endInput = $("#Enddate"),
+     hoursInput=$("#Hours");
+
+    hoursform.submit(function(event) {
+        event.preventDefault();
+        var userName = userNameInput.val();
+        var start = startInput.val();
+        var end = endInput.val();
+        var hours = hoursInput.val();
+        var requestConfig = {
+            method: "POST",
+            url: "/employee/employeehrs",
+            contentType: "application/json",
+            data: JSON.stringify({
+              userName: userName,
+              start: start,
+              end: end,
+              hours:hours
+            }),
+            error: function(response){
+                //alert('Working Hours NOT updated,please check the information');
+                alert("failed to update")
+            },
+            success: function(response){
+                alert('Working Hours Updated Successfuly');
+               }
+        };
+        $.ajax(requestConfig).then(function(responseMessage) {
+            console.log(responseMessage);
+        });
+        });
+            
+  
+    
+});
